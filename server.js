00bb0b365558c3afeafb1c69b17ff45b5816f19f
Yehor <egorkaishchenko@gmail.com>
2023-06-07 18:04:21 +0300

    deploying on server

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 const app = express();
 const path = require("path");
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
