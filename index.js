12d352cd5e696ea55654f7f7f14ed4026235aefd
Yehor <egorkaishchenko@gmail.com>
2023-06-08 19:06:57 +0300

    deploying on server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./server');
 const server = require('http').createServer(app);
 const io = require('./socket').initSocketIO(server); // Передаем сервер в socket.js
 
-const PORT = 81;
+const PORT = 3001;
 
 server.listen(PORT, () => {
   console.log(`Сервер слушает на порту ${PORT}`);
